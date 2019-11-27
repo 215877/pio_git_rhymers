4f05301a32ad84809a32565b026e890d77201cf8
Bartłomiej Skura <bartek33s@wp.pl>
2019-11-27 13:15:27 +0100

    Poprawa błędów związanych z formatowaniem kodu źródłowego, testowanie kombinacji klawiszy alt i strzałki

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,16 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;//źle sformatowany
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())//źle sformatowany
 			totalRejected++;
-			else
-				super.countIn(in);
+		else//źle sformatowany
+			super.countIn(in);//źle sformatowany
 	}
 }
